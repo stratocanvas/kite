df113d1c506386f9e0a82d98cb8eea934a8ceb25
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-05-05 01:18:24 +0900

    Add edge runtime support

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,7 +1,7 @@
 "use server";
 import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
-
+export const runtime = 'edge';
 export async function GET(request: Request) {
 	const { searchParams, origin } = new URL(request.url);
 	const code = searchParams.get("code");
