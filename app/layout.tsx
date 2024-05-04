699978b9230566c726606e0b3d5502e283027d30
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-05-05 05:38:56 +0900

    Fix dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
     <>
       <html lang="ko" suppressHydrationWarning>
         <head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         </head>
         <body className={Pretendard.className}>
           <ThemeProvider
