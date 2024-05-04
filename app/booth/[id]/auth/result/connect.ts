6a9a642f136009989d1b3dd527c2e9151d1c2b02
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-05-05 01:27:24 +0900

    Ah please

--- app/booth/[id]/auth/result/connect.ts
+++ app/booth/[id]/auth/result/connect.ts
@@ -1,6 +1,5 @@
 "use server";
 import { createClient } from "@/utils/supabase/server";
-export const runtime = 'edge';
 export async function ConnectAuthor(search: string) {
 	const supabase = createClient();
 	const { data: userData, error: userError } = await supabase.auth.getUser();
