b09c03966a2510619e13623e60b4cdc60a1b1585
AkeboshiHimari <admin@stratocanvas.com>
2024-05-10 11:53:39 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,27 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+	return await updateSession(request);
 }
 
 export const config = {
-  matcher: [
-
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
-}
+	matcher: [
+		"/auth/:path*",
+		"/write/add",
+		"/dashboard/:path*",
+		"/dashboard",
+		"/dashboard/",
+		"/dashboard?:path*",
+		"/api/:path*",
+    "/booth/:path*",
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico (favicon file)
+		 * Feel free to modify this pattern to include more paths.
+		 */
+		"/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+	],
+};
