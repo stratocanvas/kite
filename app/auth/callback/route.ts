2d5fa0bb3ba04acec22d7553be5323295a8ac4fa
AkeboshiHimari <admin@stratocanvas.com>
2024-05-10 10:36:51 +0900

    Add auth error page

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -68,9 +68,9 @@ export async function GET(request: Request) {
 			return NextResponse.redirect(`${origin}${decodeURIComponent(next)}`);
 		}
 		// 오류 페이지로 리디렉션
-		return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+		return NextResponse.redirect(`${origin}/auth/error`);
 	}
 
 	// return the user to an error page with instructions
-	return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+	return NextResponse.redirect(`${origin}/auth/error`);
 }
