d3aebeee3db123eab53682311b3076a672eb0973
AkeboshiHimari <admin@stratocanvas.com>
2024-05-13 20:28:21 +0900

    Optimize middleware

--- middleware.ts
+++ middleware.ts
@@ -5,12 +5,13 @@ export async function middleware(request: NextRequest) {
 	return await updateSession(request);
 }
 
+
 export const config = {
 	matcher: [
 		"/write/:path*",
 		"/dashboard/:path*",
-		"/api/auth/:path*",
 		"/auth/:path*",
+		"/api/auth/:path*",
 		/*
 		 * Match all request paths except for the ones starting with:
 		 * - _next/static (static files)
@@ -18,6 +19,6 @@ export const config = {
 		 * - favicon.ico (favicon file)
 		 * Feel free to modify this pattern to include more paths.
 		 */
-		"/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+		"/((?!\\/$|_next/static|_next/image|favicon.ico|event|booth|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
 	],
 };
