db8758147651cf3e76adf0f5b0576121b27531c5
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-08-02 10:01:41 +0900

    Resolve mongodb connection issue on booth page

--- app/booth/[id]/fetch.ts
+++ app/booth/[id]/fetch.ts
@@ -4,6 +4,7 @@ import { ObjectId } from "mongodb";
 
 export const GetBooth = cache(async (boothId: string) => {
 	const client = await connectDB;
+	await client.connect();
 	const db = client.db("kiteapp");
 	const data = await db
 		.collection("booth")
