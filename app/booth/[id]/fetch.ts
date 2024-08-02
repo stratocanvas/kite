2bb1bc6abf0035b67f844afa52d51fa668f57f8e
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-08-02 12:39:37 +0900

    Add revalidate 86400 in booth page to implement ISR

--- app/booth/[id]/fetch.ts
+++ app/booth/[id]/fetch.ts
@@ -2,12 +2,18 @@ import { cache } from "react";
 import { connectDB } from "@/utils/mongodb/database";
 import { ObjectId } from "mongodb";
 
+export const revalidate = 86400;
+
 export const GetBooth = cache(async (boothId: string) => {
 	const client = await connectDB;
-	await client.connect();
-	const db = client.db("kiteapp");
-	const data = await db
-		.collection("booth")
-		.findOne({ _id: new ObjectId(boothId) });
-	return data;
+	try {
+		await client.connect();
+		const db = client.db("kiteapp");
+		const data = await db
+			.collection("booth")
+			.findOne({ _id: new ObjectId(boothId) });
+		return data;
+	} finally {
+		await client.close();
+	}
 });
