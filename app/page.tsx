beebc9ca8230c34082cf85b03fe20e5203a3a873
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-08-03 13:09:35 +0900

    refactor: Change main page to server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-'use client'
 import SearchBar from "@/components/search/search";
 import * as React from "react";
-
-import QueryProvider from "@/components/search/queryprovider"; // Adjust the import path as necessary
+import QueryProvider from "@/components/search/queryprovider";
 
 export default function Home() {
-  return (
-    <QueryProvider>
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <div className="flex mx-10 justify-center items-start mt-[20vh]">
-          <SearchBar />
-        </div>
-      </React.Suspense>
-    </QueryProvider>
-  );
+	return (
+		<QueryProvider>
+			<React.Suspense>
+				<div className="flex mx-10 justify-center items-start mt-[20vh]">
+					<SearchBar />
+				</div>
+			</React.Suspense>
+		</QueryProvider>
+	);
 }
