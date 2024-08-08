6e50683e160eac4bb50e8a89f9ca9439914ca0bf
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-08-08 18:17:06 +0900

    refactor: Optimize MongoDB connection

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,13 +1,12 @@
 //이 쿼리는 클라이언트 컴포넌트에서 실행되므로, Tanstack Query를 사용하기 위해 Route Handler를 사용합니다.
-import { connectDB } from "@/utils/mongodb/database";
+import clientPromise from "@/utils/mongodb/database";
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-	const client = await connectDB;
+	const client = await clientPromise;
 	const searchParams = request.nextUrl.searchParams;
 	const query = searchParams.get("query");
 	try {
-		await client.connect();
 		const database = client.db("kiteapp");
 		const collection = database.collection("tag");
 		const searchQuery = [];
@@ -43,13 +42,12 @@ export async function GET(request: NextRequest) {
 		}
 		const results = await collection.aggregate(searchQuery).limit(10).toArray();
 		console.log(results);
+		
 		return NextResponse.json(results);
 	} catch (error) {
 		return NextResponse.json(
 			{ error: "Internal Server Error" },
 			{ status: 500 },
 		);
-	} finally {
-		await client.close();
 	}
 }
