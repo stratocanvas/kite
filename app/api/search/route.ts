7d09bc801a19d5adf190077d66048af05f8c8d74
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-08-09 22:58:32 +0900

    refactor: Combine scattered identical APIs

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,5 +1,5 @@
 //이 쿼리는 클라이언트 컴포넌트에서 실행되므로, Tanstack Query를 사용하기 위해 Route Handler를 사용합니다.
-import clientPromise from "@/utils/mongodb/database";
+import clientPromise from "@/lib/database";
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
