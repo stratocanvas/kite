55b5879b7b1e12b0701566b583017989fca3737a
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-08-13 01:18:22 +0900

    build: Merge projects into monorepo

---
+++ apps/kiteapp/next.config.js
@@ -0,0 +1,53 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	swcMinify: true,
+};
+//const withPWA = require("next-pwa")({
+//  dest: "public",
+//});
+module.exports = nextConfig;
+//module.exports = withPWA(nextConfig);
+
+module.exports = {
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+	},
+	typescript: {
+		// !! WARN !!
+		// Dangerously allow production builds to successfully complete even if
+		// your project has type errors.
+		// !! WARN !!
+		ignoreBuildErrors: true,
+	},
+	trailingSlash: false,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "api.kitebooth.com",
+				port: "",
+				pathname: "/storage/v1/object/public/**",
+			},
+			{
+				protocol: "https",
+				hostname: "kiteapp.s3.ap-northeast-2.amazonaws.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "www.kitebooth.com",
+				port: "",
+				pathname: "/api/og/booth/**",
+			},
+		],
+	},
+  experimental: {
+		serverComponentsExternalPackages: [
+			"@aws-sdk/*",
+			"@smithy+util-retry@3.0.3",
+      '@aws-sdk+middleware-sdk-s3@3.626.0'
+		],
+	},
+};
