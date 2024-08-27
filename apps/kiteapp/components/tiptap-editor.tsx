67f5ff300e67f609aa5666a23f38aa022180d6a4
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-08-27 17:25:22 +0900

    fix: Update Tiptap to use object URL

--- apps/kiteapp/components/tiptap-editor.tsx
+++ apps/kiteapp/components/tiptap-editor.tsx
@@ -68,8 +68,7 @@ const Tiptap = ({ onChange, initValue }: TiptapProps) => {
 		extensions: [
 			Placeholder.configure({
 				// Use a placeholder:
-				placeholder: `인포 이미지를 꼭 올려주세요!
-세로 길이가 8192보다 큰 이미지는 화질이 저하될 수 있습니다. 긴 인포 이미지 업로드시 주의해 주세요.`,
+				placeholder: '인포 이미지를 올려주세요.',
 				emptyEditorClass: "is-editor-empty",
 			}),
 			StarterKit.configure({
@@ -125,7 +124,7 @@ const Tiptap = ({ onChange, initValue }: TiptapProps) => {
 								.insertContentAt(pos, {
 									type: "image",
 									attrs: {
-										src: fileReader.result,
+										src: URL.createObjectURL(file),
 									},
 								})
 								.focus()
@@ -151,7 +150,7 @@ const Tiptap = ({ onChange, initValue }: TiptapProps) => {
 								.insertContentAt(currentEditor.state.selection.anchor, {
 									type: "image",
 									attrs: {
-										src: fileReader.result,
+										src: URL.createObjectURL(file),
 									},
 								})
 								.focus()
