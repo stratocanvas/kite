7459a935ac52444cab206006b5d83955b8f22794
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-08-29 03:45:38 +0900

    chore: Remove user.role from session

--- apps/kiteapp/lib/auth/auth.ts
+++ apps/kiteapp/lib/auth/auth.ts
@@ -7,7 +7,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 	callbacks: {
 		async session({ session, token }) {
 			session.user.id = token.sub as string;
-			session.user.role = token.role as string;
 			session.user.email = undefined;
 
 			return session;
