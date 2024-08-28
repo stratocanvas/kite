74576e4a1a5ffbce0ad11b91d80399871496df5b
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-08-28 17:10:28 +0900

    chore: Move DynamoDB client settings to db file

--- apps/kite-auth/lib/auth/auth.ts
+++ apps/kite-auth/lib/auth/auth.ts
@@ -1,29 +1,12 @@
 import NextAuth from "next-auth";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import { findUser, initializeUser, linkProfile } from "./account";
-import { DynamoDB, type DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+
 import { DynamoDBAdapter } from "@auth/dynamodb-adapter";
 import Twitter from "next-auth/providers/twitter";
 import Google from "next-auth/providers/google";
+import client from "../db";
 
 const isProduction = process.env.NODE_ENV === "production";
-
-const config: DynamoDBClientConfig = {
-	credentials: {
-		accessKeyId: process.env.AUTH_DYNAMODB_ID || "",
-		secretAccessKey: process.env.AUTH_DYNAMODB_SECRET || "",
-	},
-	region: process.env.AUTH_DYNAMODB_REGION,
-};
-
-const client = DynamoDBDocument.from(new DynamoDB(config), {
-	marshallOptions: {
-		convertEmptyValues: true,
-		removeUndefinedValues: true,
-		convertClassInstanceToMap: true,
-	},
-});
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	trustHost: true,
--- apps/kite-auth/lib/db.ts
+++ apps/kite-auth/lib/db.ts
@@ -1,37 +1,20 @@
-// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { DynamoDB, type DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 
-if (!process.env.MONGODB_URI) {
-	throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-}
-
-const uri = process.env.MONGODB_URI;
-const options = {
-	serverApi: {
-		version: ServerApiVersion.v1,
-		strict: true,
-		deprecationErrors: true,
+const config: DynamoDBClientConfig = {
+	credentials: {
+		accessKeyId: process.env.AUTH_DYNAMODB_ID || "",
+		secretAccessKey: process.env.AUTH_DYNAMODB_SECRET || "",
 	},
+	region: process.env.AUTH_DYNAMODB_REGION,
 };
 
-let client: MongoClient;
+const client = DynamoDBDocument.from(new DynamoDB(config), {
+	marshallOptions: {
+		convertEmptyValues: true,
+		removeUndefinedValues: true,
+		convertClassInstanceToMap: true,
+	},
+});
 
-if (process.env.NODE_ENV === "development") {
-	// In development mode, use a global variable so that the value
-	// is preserved across module reloads caused by HMR (Hot Module Replacement).
-	const globalWithMongo = global as typeof globalThis & {
-		_mongoClient?: MongoClient;
-	};
-
-	if (!globalWithMongo._mongoClient) {
-		globalWithMongo._mongoClient = new MongoClient(uri, options);
-	}
-	client = globalWithMongo._mongoClient;
-} else {
-	// In production mode, it's best to not use a global variable.
-	client = new MongoClient(uri, options);
-}
-
-// Export a module-scoped MongoClient. By doing this in a
-// separate module, the client can be shared across functions.
-export default client;
+export default client
