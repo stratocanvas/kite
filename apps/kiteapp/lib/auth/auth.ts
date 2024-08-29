81612600b7bfec1f58988779a4d683636aef17cc
Stratocanvas <156805377+AkeboshiHimari@users.noreply.github.com>
2024-08-30 03:02:56 +0900

    chore: Merge auth.config.ts into auth.ts

--- apps/kiteapp/lib/auth/auth.ts
+++ apps/kiteapp/lib/auth/auth.ts
@@ -3,7 +3,60 @@ import authConfig from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	session: { strategy: "jwt" },
-	...authConfig,
+	trustHost: true,
+	adapter: DynamoDBAdapter(client),
+	providers: [
+		Google({
+			authorization: {
+				params: {
+					scope: "openid profile",
+					access_type: "offline",
+					prompt: "consent",
+				},
+			},
+		}),
+		Twitter({
+			authorization: {
+				params: {
+					scope: "offline.access",
+				},
+			},
+		}),
+	],
+	cookies: {
+		sessionToken: {
+			name: `${isProduction ? "__Secure-" : ""}authjs.session-token`,
+			options: {
+				httpOnly: true,
+				sameSite: "lax",
+				path: "/",
+				secure: isProduction,
+				domain: isProduction
+					? `.${process.env.NEXT_PUBLIC_BASE_URL}`
+					: "localhost",
+			},
+		},
+		callbackUrl: {
+			name: `${isProduction ? "__Secure-" : ""}authjs.callback-url`,
+			options: {
+				sameSite: "lax",
+				path: "/",
+				secure: isProduction,
+				domain: isProduction
+					? `.${process.env.NEXT_PUBLIC_BASE_URL}`
+					: "localhost",
+			},
+		},
+		csrfToken: {
+			name: "__Host-authjs.csrf-token",
+			options: {
+				httpOnly: true,
+				sameSite: "lax",
+				path: "/",
+				secure: isProduction,
+			},
+		},
+	},
 	callbacks: {
 		async session({ session, token }) {
 			session.user.id = token.sub as string;
