e1feb9273e080633949b8c4cc0c80830380d65f3
Stratocanvas <156805377+AkeboshiHimari@users.noreply.github.com>
2024-08-30 03:03:27 +0900

    chore: Change test session display layout

--- apps/kiteapp/app/page.tsx
+++ apps/kiteapp/app/page.tsx
@@ -1,18 +1,18 @@
 import SearchBar from "@/components/search/search";
 import * as React from "react";
 import QueryProvider from "@/components/search/queryprovider";
-import { auth } from "@/lib/auth/auth"
+import { auth } from "@/lib/auth/auth";
 
 export default async function Home() {
-	const session = await auth()
+	const session = await auth();
 
 	return (
 		<QueryProvider>
 			<React.Suspense>
 				<div className="flex mx-auto justify-center items-center mt-[20vh] w-4/5 md:w-1/2 lg:w-2/5 xl:w-1/3">
-					<SearchBar/>
+					<SearchBar />
 				</div>
-				{session ? (JSON.stringify(session)) : ('')}
+				{session ? <pre>{JSON.stringify(session, null, 2)}</pre> : ""}
 			</React.Suspense>
 		</QueryProvider>
 	);
