38b38bd455ae866ede930e03bccac9d7ec88d523
Stratocanvas <156805377+AkeboshiHimari@users.noreply.github.com>
2024-09-02 14:42:29 +0900

    style: change tab width to 2

--- apps/kite-auth/components/signin.tsx
+++ apps/kite-auth/components/signin.tsx
@@ -1,21 +1,21 @@
-import { Button } from "./ui/button";
-import Image from "next/image";
-import GoogleLogo from "@/public/google.svg";
-import XLogo from "@/public/x.svg";
-import { signIn, signOut } from "@/lib/auth/auth";
-import { LogOut } from "lucide-react";
-import { headers } from "next/headers";
+import { Button } from './ui/button'
+import Image from 'next/image'
+import GoogleLogo from '@/public/google.svg'
+import XLogo from '@/public/x.svg'
+import { signIn, signOut } from '@/lib/auth/auth'
+import { LogOut } from 'lucide-react'
+import { headers } from 'next/headers'
 
 export function SignIn({ provider }: { provider: string }) {
-	const size = provider === "google" ? 30 : 24;
+  const size = provider === 'google' ? 30 : 24
 
-	return (
-		<form
-			action={async () => {
-				"use server";
-				const headersList = headers();
-				const referer = headersList.get("referer");
-				let next = "";
+  return (
+    <form
+      action={async () => {
+        'use server'
+        const headersList = headers()
+        const referer = headersList.get('referer')
+        let next = ''
 
 				if (referer) {
 					try {
@@ -47,30 +47,30 @@ export function SignIn({ provider }: { provider: string }) {
 }
 
 export function Link({ provider }: { provider: string }) {
-	return (
-		<form
-			action={async () => {
-				"use server";
-				await signIn(provider);
-			}}
-		>
-			<Button variant="default">연결</Button>
-		</form>
-	);
+  return (
+    <form
+      action={async () => {
+        'use server'
+        await signIn(provider)
+      }}
+    >
+      <Button variant="default">연결</Button>
+    </form>
+  )
 }
 
 export function SignOut() {
-	return (
-		<form
-			action={async () => {
-				"use server";
-				await signOut({ redirectTo: "/" });
-			}}
-		>
-			<Button type="submit" className="w-full" variant="secondary" size="lg">
-				<LogOut className="h-4 w-4 mr-2" />
-				로그아웃
-			</Button>
-		</form>
-	);
+  return (
+    <form
+      action={async () => {
+        'use server'
+        await signOut({ redirectTo: '/' })
+      }}
+    >
+      <Button type="submit" className="w-full" variant="secondary" size="lg">
+        <LogOut className="h-4 w-4 mr-2" />
+        로그아웃
+      </Button>
+    </form>
+  )
 }
