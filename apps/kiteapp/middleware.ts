8fecf4ca1d856eff9045cd1ff6d3a1576cb541e0
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-09-04 10:42:35 +0900

    update: Modify RBAC redirect

--- apps/kiteapp/middleware.ts
+++ apps/kiteapp/middleware.ts
@@ -3,24 +3,27 @@ import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 
 export default auth((req) => {
-	console.log('middleware')
-	const headersList = headers();
-	const host = headersList.get("host") || "localhost:3001";
-	const proto = headersList.get("x-forwarded-proto") || "http";
-	const currentUrl = `${proto}://${host}${req.nextUrl.pathname}${req.nextUrl.search}`;
+	console.log("middleware");
+	const currentUrl = req.nextUrl.pathname;
 
 	const signInUrl = new URL(
 		process.env.NEXT_PUBLIC_AUTH_URL || "http://localhost:3001",
 	);
 
-	const protectedRoutes = ["/write/add", "/dashboard", '/api/cart', '/api/bookmark'];
+	const protectedRoutes = [
+		"/write/add",
+		"/dashboard",
+		"/api/cart",
+		"/api/bookmark",
+	];
 
 	if (
 		!req.auth &&
 		protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))
 	) {
 		signInUrl.searchParams.set("next", currentUrl);
-		return Response.redirect(signInUrl);
+		signInUrl.searchParams.set("service", "kite");
+		return NextResponse.redirect(signInUrl);
 	}
 
 	return NextResponse.next();
